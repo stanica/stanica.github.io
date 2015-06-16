9d97aebc1e97b76a18b3f7b917c855add9630152
stanica <robert@stani.ca>
2015-06-15 22:28:04 -0400

    Finish P6

--- p6/jasmine/spec/feedreader.js
+++ p6/jasmine/spec/feedreader.js
@@ -51,7 +51,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-	describe('The menu', function() {
+	describe('The Menu', function() {
 		
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -74,21 +74,47 @@ $(function() {
 			$('.menu-icon-link').trigger('click');
 			expect($('body').hasClass('menu-hidden')).toBe(true);
 		});
-		
+	});
+	
     /* TODO: Write a new test suite named "Initial Entries" */
-
+	describe('Initial Entries', function() {
+		
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
-    /* TODO: Write a new test suite named "New Feed Selection"
-
+		beforeEach(function(done) {
+			loadFeed(0, function() {
+				done();
+			});
+		});
+		 
+		it('contain at least one entry', function(done) {
+			expect($('.feed')[0].children.length).toBeGreaterThan(0);
+			done();
+		});
+	});
+	
+    /* TODO: Write a new test suite named "New Feed Selection" */
+	describe('New Feed Selection', function() {
+		
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+		beforeEach(function(done) {
+			loadFeed(3, function() {
+				done();
+			});
+			this.content = $('.feed').find('h2')[0];
+		});	
+			
+		it('changes content', function(done) {
+			this.content2 = $('.feed').find('h2')[0];	
+			expect(this.content).not.toBe(this.content2);
+			done();
+		});
 	});
 }());

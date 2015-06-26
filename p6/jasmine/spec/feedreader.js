2576d7fd01b7f55f6089f322d44a844b99a185de
stanica <robert@stanica.ca>
2015-06-26 15:06:30 -0400

    Remove trailing white spaces

--- p6/jasmine/spec/feedreader.js
+++ p6/jasmine/spec/feedreader.js
@@ -22,7 +22,7 @@ $(function() {
 			expect(allFeeds).toBeDefined();
 			expect(allFeeds.length).not.toBe(0);
 		});
-		
+
 		/* Test that loops through each feed in the allFeeds object and ensures it has a URL defined
 		* and that the URL is not empty.
 		*/
@@ -32,7 +32,7 @@ $(function() {
 				expect(feed.url.length).not.toBe(0);
 			});
 		});
-		
+
 		/* Test that loops through each feed in the allFeeds object and ensures it has a name defined
 		* and that the name is not empty.
 		*/
@@ -42,7 +42,7 @@ $(function() {
 				expect(feed.name.length).not.toBe(0);
 			});
 		});
-		
+
 		/* Test that adds additional link to list of feeds
 		*/
 		it('can be increased', function() {
@@ -56,14 +56,14 @@ $(function() {
 
 
 	describe('The Menu', function() {
-		
+
 		/* Test that ensures the menu element is  hidden by default.
 		*/
 		it('is hidden by default', function() {
 			expect($('body').attr('class')).toBeDefined();
 			expect($('body').hasClass('menu-hidden')).toBe(true);
 		});
-		
+
 		/* test that ensures the menu changes visibility when the menu icon is clicked.
 		*/
 		it('changes visibility when clicked', function() {
@@ -73,25 +73,25 @@ $(function() {
 			expect($('body').hasClass('menu-hidden')).toBe(true);
 		});
 	});
-	
+
 	describe('Initial Entries', function() {
-		
+
 		/* Test that ensures when the loadFeed function is called and completes its work, there is at least
 		* a single .entry element within the .feed container.
 		*/
 		beforeEach(function(done) {
 			loadFeed(0, done);
 		});
-		 
+
 		it('contain at least one entry', function() {
 			expect($('.feed')[0].children.length).toBeGreaterThan(0);
 		});
 	});
-	
-	describe('New Feed Selection', function() {
+
+    describe('New Feed Selection', function() {
 		var content = '';
 		var	content2 = '';
-        
+
 		beforeEach(function(done) {
             $('.feed').empty();
 			loadFeed(0, function() {
@@ -101,18 +101,18 @@ $(function() {
 					done();
 				});
 			});
-		});	
-		
+		});
+
 		/* Test that ensures when a new feed is loaded by the loadFeed function that the content actually changes.
 		*/
 		it('changes content', function() {
             expect(content).not.toBe(content2);
 		});
 	});
-	
+
 	/* Test suite to test header text of page */
 	describe('Header Title', function() {
-		
+
 		/* Test to ensure header text changes when menu item is clicked */
 		it('changes to reflect menu selection', function() {
 			var original = $('.header-text');
